7d99d4f445f44f8c5f7b2f98555c7f2379605e9a
THEKINGSTAR <thekingstar5002@gmail.com>
2023-04-26 01:03:07 +0200

    testng zin code

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+/**
+ * main- Entry point of the program.
+ * Return: state of exit.
+ */
+int main(void)
+{
+	char command[20];
+	char *arg[] = {NULL, NULL};
+	const char *envp[] = {NULL};
+
+	while (1)
+	{
+		printf(" ($) ");
+		scanf("%s", command);
+		/*printf("\n%s\n", command);*/
+		arg[0] = command;
+
+		if (fork() == 0)
+		{
+			execve(arg[0], arg, (char * const *)envp);
+			perror("hsh");
+			exit(EXIT_FAILURE);
+		}
+		wait(NULL);
+	}
+	return (0);
+}
